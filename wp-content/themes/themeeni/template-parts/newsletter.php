5df16468043d533d9f11209fb10d4ac7c477c5ed
dsanchez <dsanchez@campus-eni.fr>
2025-08-27 14:16:18 +0200

    Ajout d'un template pour s'inscrire à la newsletter

---
+++ wp-content/themes/themeeni/template-parts/newsletter.php
@@ -0,0 +1,5 @@
+<form class="newsletter-form">
+    <p>Si tu veux rester informé des sorties cinés de l'ENI, Abonne toi à la newsletter !</p>
+    <input type="email" name="email" placeholder="Saisissez votre email">
+    <input type="submit" value="Yes ! je m'abonne">
+</form>
