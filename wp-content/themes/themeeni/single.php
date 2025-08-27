a2f5fa1dc607d0575ed77b9de5f33cb06d9312c7
dsanchez <dsanchez@campus-eni.fr>
2025-08-27 12:29:36 +0200

    creation des fichiers du theme archive home page et single

--- wp-content/themes/themeeni/single.php
+++ wp-content/themes/themeeni/single.php
@@ -1,4 +1,4 @@
-//correspond aux articles - contenu chaud.
+<?php //correspond aux articles - contenu chaud. ?>
 <?php get_header(); ?>
 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
     <article class="post">
