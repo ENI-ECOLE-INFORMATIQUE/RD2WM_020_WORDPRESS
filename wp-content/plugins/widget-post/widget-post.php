8ae6e0df177e17c4c03618f52ff71af04f315cdc
dsanchez <dsanchez@campus-eni.fr>
2025-09-01 12:27:12 +0200

    Ajout d'un shortcode

--- wp-content/plugins/widget-post/widget-post.php
+++ wp-content/plugins/widget-post/widget-post.php
@@ -123,7 +123,7 @@ class Widget_Post extends WP_Widget {
 }
 
 function my_shortcode($attributes,$content,$shortcodeTag){
-    $html='<p>Bonjour '.($attributes['name']??'').' depuis le shortcode '.$shortcodeTag.'. '.$content.'</p>';
+    $html='<p>Bonjour '.($attributes['name']??'').' depuis le shortcode '.$shortcodeTag.'.<br> '.$content.'</p>';
     return $html;
 }
 
